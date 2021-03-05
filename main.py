d8326a3d596e065185430b89c6881108a19ddbba
Adam Medveczki <grizzancs@gmail.com>
2021-03-05 14:29:15 +0100

    Before refactoring curve detect functions into usable class

--- main.py
+++ main.py
@@ -31,200 +31,27 @@
 # 8. Draw lanes and fill with color
 
 from calibration import calibration
+from thresholds import color_sobel_threshold
+from perspective import perspective
 import matplotlib.image as mpimg
 import glob
 
 
-
 import numpy as np
 import cv2
-#import matplotlib.image as mpimg
-
-# cv2.COLOR_RGB2HLS, 2,
-
-def abs_sobel_thresh(img, sobel_kernel=3, thresh=(0, 255), orient='x'):
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    # 105, 255 L, c2 (de van benne tul sok vilagos)
-# 160, 255 c3
-    #mpimg.imsave("fancy/channel0.jpg", grayto3(hls[:,:,0]))
-    #mpimg.imsave("fancy/channel1.jpg", grayto3(hls[:,:,1]))
-    #mpimg.imsave("fancy/channel2.jpg", grayto3(hls[:,:,2]))
-    
-    l = hls[:,:,1] * color_thresh(hls, 1, (105, 255)) * 0.6
-    s = hls[:,:,2] * color_thresh(hls, 2, (160, 255)) * 1.2
-    gray = np.maximum(np.uint8(s), np.uint8(l))
-    gray2 = grayto3(gray)
-    if orient == 'x':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
-    else:
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
-
-    abs_sobel = np.absolute(sobel)
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-
-    binary_output = np.zeros_like(scaled_sobel)
-    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-
-    return binary_output
-
-def grayto3(c):
-    return np.dstack((c,c,c))
-
-def color_thresh(img, channel_index, thresh):
-    channel = img[:,:,channel_index]   
-    # Threshold color channel
-    bin_color = np.zeros_like(channel)
-    bin_color[(channel >= thresh[0]) & (channel <= thresh[1])] = 1
-    #binchannel = channel * bin_color
-    #fancy_image = np.dstack((binchannel, binchannel, binchannel))
-    #mpimg.imsave("fancy/channel_%d_thr%d_%d.jpg" % (channel_index, thresh[0], thresh[1]), fancy_image)
-
-    return bin_color
-
-def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
-    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    # 105, 255 L, c2 (de van benne tul sok vilagos)
-# 160, 255 c3
-    #mpimg.imsave("fancy/channel0.jpg", grayto3(hls[:,:,0]))
-    #mpimg.imsave("fancy/channel1.jpg", grayto3(hls[:,:,1]))
-    #mpimg.imsave("fancy/channel2.jpg", grayto3(hls[:,:,2]))
-    
-    l = hls[:,:,1] * color_thresh(hls, 1, (105, 255)) * 0.6
-    s = hls[:,:,2] * color_thresh(hls, 2, (160, 255)) * 1.2
-    gray = np.maximum(np.uint8(s), np.uint8(l))
-    gray2 = grayto3(gray)
-    # mpimg.imsave("fancy/prepgray_thr%d_%d.jpg" % (mag_thresh[0], mag_thresh[1]), gray2)
-
-    
-    
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
-    magnitude = np.sqrt(sobelx**2 + sobely**2)
-    maxmag = np.max(magnitude)
-    scaled_sobel = np.uint8(255*magnitude/maxmag)
-    binary = np.zeros_like(gray)
-    binary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
-    return binary
-    
-def dir_thresh(image, sobel_kernel=3, thresh=(0, np.pi/2)):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)[:,:,2] # not really gray anymore
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
-    
-    ax = np.abs(sobelx)
-    ay = np.abs(sobely)
-    
-    dirgrad = np.arctan2(ay, ax)
-    binary = np.uint8(np.zeros_like(sobelx))
-    binary[(dirgrad >= thresh[0]) & (dirgrad <= thresh[1])] = 1
-    return binary
-
-def color_sobel_threshold(img, sobel_kernel, color_space, **kwargs):
-    # no thresholds by default
-    c1_threshold = c2_threshold = c3_threshold = xabs_threshold = mag_threshold = \
-        dir_threshold = None
-    
-    fancy_name = ""
-    fancy = False
-    
-    for kw in kwargs.keys():
-        if kw == "c1_threshold":
-            c1_threshold = kwargs[kw]
-        elif kw == "c2_threshold":
-            c2_threshold = kwargs[kw]
-        elif kw == "c3_threshold":
-            c3_threshold = kwargs[kw]
-        elif kw == "xabs_threshold":
-            xabs_threshold = kwargs[kw]
-        elif kw == "mag_threshold":
-            mag_threshold = kwargs[kw]
-        elif kw == "dir_threshold":
-            dir_threshold = kwargs[kw]
-        elif kw == "fancy" and kwargs[kw]:
-            fancy = True
-            continue
-        else:
-            raise Exception("Invalid kwarg: %s", kw)
-            
-        # Dir thresholds are float others are not, remove trailing zeroes at the end after %f
-        fancy_name += "_%s_%s_%s" % (kw, ('%3.2f' % kwargs[kw][0]).rstrip('0').rstrip('.'),
-                                         ('%3.2f' % kwargs[kw][1]).rstrip('0').rstrip('.'))
-   
-    # Currently color is & (and), others are | (or'd)
-    target_color = cv2.cvtColor(img, color_space)
-    bin_c1 = color_thresh(target_color, 0, c1_threshold) if c1_threshold else np.ones_like(img[:,:,0])
-    bin_c2 = color_thresh(target_color, 1, c2_threshold) if c2_threshold else np.ones_like(img[:,:,0])
-    bin_c3 = color_thresh(target_color, 2, c3_threshold) if c3_threshold else np.ones_like(img[:,:,0])
-    
-    bin_xabs = abs_sobel_thresh(img, sobel_kernel, xabs_threshold) if xabs_threshold else np.zeros_like(bin_c1)
-    bin_mag = mag_thresh(img, sobel_kernel, mag_threshold) if mag_threshold else np.zeros_like(bin_c1)
-    bin_dir = dir_thresh(img, sobel_kernel, dir_threshold)  if dir_threshold else np.zeros_like(bin_c1)
- 
-    combined = np.zeros_like(bin_c1)
-    combined[
-        (bin_c1 == 1) &
-        (bin_c2 == 1) &
-        (bin_c3 == 1) |
-        (bin_mag == 1) |
-        (bin_dir == 1) |
-        (bin_xabs == 1)] = 255
-
-
-    if fancy:
-        fancy_image = np.dstack((combined, bin_c2, bin_c3))
-        mpimg.imsave("fancy/threshold_%s.jpg" % fancy_name, fancy_image)
-
-    return combined
 
 CALIBRATION_IMAGES = "camera_cal/*.jpg"
 
 calib = calibration(True, True)
 calib.calibrate(glob.glob(CALIBRATION_IMAGES), 9, 6)
 undistorted = calib.undistort(mpimg.imread("test_images/straight_lines3.jpg"))
-# =============================================================================
-# 
-# target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2Luv)
-# mpimg.imsave("fancy/threshold_colorconvert_Luv_0_.jpg", np.dstack([target_color[:,:,0], target_color[:,:,0], target_color[:,:,0]]))
-# mpimg.imsave("fancy/threshold_colorconvert_Luv_1_.jpg", np.dstack([target_color[:,:,1], target_color[:,:,1], target_color[:,:,1]]))
-# mpimg.imsave("fancy/threshold_colorconvert_Luv_2_.jpg", np.dstack([target_color[:,:,2], target_color[:,:,2], target_color[:,:,2]]))
-# 
-# target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2Lab)
-# mpimg.imsave("fancy/threshold_colorconvert_Lab_0_.jpg", np.dstack([target_color[:,:,0], target_color[:,:,0], target_color[:,:,0]]))
-# mpimg.imsave("fancy/threshold_colorconvert_Lab_1_.jpg", np.dstack([target_color[:,:,1], target_color[:,:,1], target_color[:,:,1]]))
-# mpimg.imsave("fancy/threshold_colorconvert_Lab_2_.jpg", np.dstack([target_color[:,:,2], target_color[:,:,2], target_color[:,:,2]]))
-# 
-# target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2YCrCb)
-# mpimg.imsave("fancy/threshold_colorconvert_YCrCb_0_.jpg", np.dstack([target_color[:,:,0], target_color[:,:,0], target_color[:,:,0]]))
-# mpimg.imsave("fancy/threshold_colorconvert_YCrCb_1_.jpg", np.dstack([target_color[:,:,1], target_color[:,:,1], target_color[:,:,1]]))
-# mpimg.imsave("fancy/threshold_colorconvert_YCrCb_2_.jpg", np.dstack([target_color[:,:,2], target_color[:,:,2], target_color[:,:,2]]))
-# 
-# target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2YUV)
-# mpimg.imsave("fancy/threshold_colorconvert_IYUV_0_.jpg", np.dstack([target_color[:,:,0], target_color[:,:,0], target_color[:,:,0]]))
-# mpimg.imsave("fancy/threshold_colorconvert_IYUV_1_.jpg", np.dstack([target_color[:,:,1], target_color[:,:,1], target_color[:,:,1]]))
-# mpimg.imsave("fancy/threshold_colorconvert_IYUV_2_.jpg", np.dstack([target_color[:,:,2], target_color[:,:,2], target_color[:,:,2]]))
-# 
-#target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2HLS)
-#mpimg.imsave("fancy/threshold_colorconvert_HLS_0_.jpg", np.dstack([target_color[:,:,0], target_color[:,:,0], target_color[:,:,0]]))
-#mpimg.imsave("fancy/threshold_colorconvert_HLS_1_.jpg", np.dstack([target_color[:,:,1], target_color[:,:,1], target_color[:,:,1]]))
-#mpimg.imsave("fancy/threshold_colorconvert_HLS_2_.jpg", np.dstack([target_color[:,:,2], target_color[:,:,2], target_color[:,:,2]]))
 
-#xx = mpimg.imsave("fancy/threshold_colorconvert_HLS_12_.jpg", np.dstack([np.zeros_like(target_color[:,:,2]), target_color[:,:,1], target_color[:,:,2]]))
-# =============================================================================
 
-class perspective:
-    def __init__(self, ll, ul, ur, lr, offset, img):
-        src = np.float32([[ll, ul, ur, lr]])
-        dst = np.float32([
-            [offset, img.shape[0] - 1],
-             [offset, 0], # UL
-             [img.shape[1] - offset, 0], # UR
-             [img.shape[1] - offset, img.shape[0] - 1]
-             ])
-        self.M = cv2.getPerspectiveTransform(src, dst)
-        
-    def unwarp(self, image):
-        return cv2.warpPerspective(image, self.M, (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
-
+thresh = color_sobel_threshold(undistorted, 3, cv2.COLOR_RGB2HLS, fancy = True,
+                      mag_threshold = (30, 170),
+                      c2_threshold = (105, 255),
+                      c3_threshold = (170, 255),
+                      )
 
 PERSPECTIVE_OFFSET = 250
 
@@ -233,63 +60,195 @@ PUL = [620, 445]
 PUR = [700, 445]
 PLR = [1176, 688]
 
+persp = perspective(PLL, PUL, PUR, PLR, PERSPECTIVE_OFFSET, thresh)
+warped = persp.warp(thresh)
 
-minl = -1
-maxl = 1
-step = 1
+def find_lane_pixels_histogram(binary_warped):
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    # Create an output image to draw on and visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]//2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
+    # HYPERPARAMETERS
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
 
-curl = minl
-while curl <= maxl:
-    curr = minl
-    while curr <= maxl:
-        persp = perspective(PLL, [PUL[0] + curl, PUL[1]], [PUR[0] + curr, PUR[1]], PLR, PERSPECTIVE_OFFSET, undistorted)
-#        persp = perspective([PLL[0]+curl, PLL[1]], PUL, PUR, [PLR[0]+curr, PLR[1]], PERSPECTIVE_OFFSET, undistorted)
-        warped = persp.unwarp(undistorted)
-        mpimg.imsave("fancy/warped_%d_%d.jpg" % (curl, curr), warped)
+    # Set height of windows - based on nwindows above and image shape
+    window_height = np.int(binary_warped.shape[0]//nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated later for each window in nwindows
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
         
-        if curr == maxl:
-            break
-        curr = min(maxl, curr + step)
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+        (win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),
+        (win_xright_high,win_y_high),(0,255,0), 2) 
+        
+        # Identify the nonzero pixels in x and y within the window #
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-    if curl == maxl:
-        break
-    curl = min(maxl, curl + step)
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    try:
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+    except ValueError:
+        # Avoids an error if the above is not implemented fully
+        pass
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    return leftx, lefty, rightx, righty, out_img
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
 
+def fit_polynomial(binary_warped):
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
 
-mint = 30
-maxt = 170
-thr = 8
-mi = mint
+    left_fitx, right_fitx, ploty = fit_poly(binary_warped.shape, leftx, lefty, rightx, righty)
 
-# 105, 255 L, c2 (de van benne tul sok vilagos)
-# 160, 255 c3
+    ## Visualization ##
+    # Colors in the left and right lane regions
+    out_img[lefty, leftx] = [255, 0, 0]
+    out_img[righty, rightx] = [0, 0, 255]
 
-# 150, 255 C2 maghoz?
-fixed_max = True
+    # Plots the left and right polynomials on the lane lines
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
 
-# =============================================================================
-# while mi < maxt:
-#     ma = maxt if fixed_max else min(maxt, mi + thr)
-#     
-#     while ma <= maxt:
-#         color_sobel_threshold(undistorted, 3, cv2.COLOR_RGB2HLS, fancy = True, c3_threshold = (mi, ma))
-#        
-#         if ma == maxt:
-#             break
-#         
-#         ma = min(maxt, ma + thr)
-# 
-#     mi += thr
-# # 30, 170 mag
-# =============================================================================
-color_sobel_threshold(undistorted, 3, cv2.COLOR_RGB2HLS, fancy = True,
-                      mag_threshold = (30, 170),
-                      c2_threshold = (105, 255),
-                      c3_threshold = (170, 255),
-                      )
+    return out_img
 
+
+def fit_poly(img_shape, leftx, lefty, rightx, righty):
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    
+    
+    
+    return left_fitx, right_fitx, ploty
+
+class lane_detector:
+    STATE_INIT = 0 # first start or we are completely lost
+    STATE_LAST_FRAME_USABLE = 1 # continue using polynomial+epsilon for searching lane instead of boxes
+    STATE_UNCERTAIN = 2 # until MAX_UNCERTAIN_FRAMES we hold onto previous curvature
+    MAX_UNCERTAIN_FRAMES = 5 # TODO test
+    
+    def __init__(self):
+        self.state = STATE_INIT
+        
+    def process(self, warped):
+        
+
+def search_around_poly(binary_warped):
+    margin = 100
+
+    # Grab activated pixels
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
+                    left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
+                    left_fit[1]*nonzeroy + left_fit[2] + margin)))
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
+                    right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
+                    right_fit[1]*nonzeroy + right_fit[2] + margin)))
+    
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit new polynomials
+    left_fitx, right_fitx, ploty = fit_poly(binary_warped.shape, leftx, lefty, rightx, righty)
+    
+    ## Visualization ##
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+                              ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                              ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    
+    # Plot the polynomial lines onto the image
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    ## End visualization steps ##
+    
+    return result
+
+mpimg.imsave("fancy/warped.jpg", warped)
+
+out_img = fit_polynomial(warped)
+
+plt.imshow(out_img)
 
 #perspective ll ul ur lr (255, 677), (597, 448), (687, 448), (1051, 677)
 
