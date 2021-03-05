e9a8e1d332901694ae15c6562fb145bfe93432b0
Adam Medveczki <grizzancs@gmail.com>
2021-03-05 10:56:55 +0100

    Perspective seems OK, will put it into a separate file

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ CALIBRATION_IMAGES = "camera_cal/*.jpg"
 
 calib = calibration(True, True)
 calib.calibrate(glob.glob(CALIBRATION_IMAGES), 9, 6)
-undistorted = calib.undistort(mpimg.imread("test_images/test5.jpg"))
+undistorted = calib.undistort(mpimg.imread("test_images/straight_lines3.jpg"))
 # =============================================================================
 # 
 # target_color = cv2.cvtColor(undistorted, cv2.COLOR_RGB2Luv)
@@ -211,12 +211,57 @@ undistorted = calib.undistort(mpimg.imread("test_images/test5.jpg"))
 #xx = mpimg.imsave("fancy/threshold_colorconvert_HLS_12_.jpg", np.dstack([np.zeros_like(target_color[:,:,2]), target_color[:,:,1], target_color[:,:,2]]))
 # =============================================================================
 
+class perspective:
+    def __init__(self, ll, ul, ur, lr, offset, img):
+        src = np.float32([[ll, ul, ur, lr]])
+        dst = np.float32([
+            [offset, img.shape[0] - 1],
+             [offset, 0], # UL
+             [img.shape[1] - offset, 0], # UR
+             [img.shape[1] - offset, img.shape[0] - 1]
+             ])
+        self.M = cv2.getPerspectiveTransform(src, dst)
+        
+    def unwarp(self, image):
+        return cv2.warpPerspective(image, self.M, (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
+
+
+PERSPECTIVE_OFFSET = 250
+
+PLL = [344, 688]
+PUL = [620, 445]
+PUR = [700, 445]
+PLR = [1176, 688]
+
+
+minl = -1
+maxl = 1
+step = 1
+
+
+curl = minl
+while curl <= maxl:
+    curr = minl
+    while curr <= maxl:
+        persp = perspective(PLL, [PUL[0] + curl, PUL[1]], [PUR[0] + curr, PUR[1]], PLR, PERSPECTIVE_OFFSET, undistorted)
+#        persp = perspective([PLL[0]+curl, PLL[1]], PUL, PUR, [PLR[0]+curr, PLR[1]], PERSPECTIVE_OFFSET, undistorted)
+        warped = persp.unwarp(undistorted)
+        mpimg.imsave("fancy/warped_%d_%d.jpg" % (curl, curr), warped)
+        
+        if curr == maxl:
+            break
+        curr = min(maxl, curr + step)
+
+    if curl == maxl:
+        break
+    curl = min(maxl, curl + step)
+
+
 
 mint = 30
 maxt = 170
 thr = 8
 mi = mint
-
 
 # 105, 255 L, c2 (de van benne tul sok vilagos)
 # 160, 255 c3
